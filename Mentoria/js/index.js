92f1c3cfcc3a0a60cab497e5694166cd4b588303
Giovanna Cavalcanti de Lima <95053005+GiovannaCavalcanti@users.noreply.github.com>
2022-01-09 01:27:08 -0300

    Versão Final

---
+++ Mentoria/js/index.js
@@ -0,0 +1,15 @@
+var nScroll = $(window).scrollTop();
+
+/* Função de click no botao para Scroll para Inscrição */
+function scrollWin(x, y) {
+  window.scrollBy(x, y);
+}
+/* Função para Conferir se houve o Scroll */
+$(document).scroll(function () {
+  if ((nScroll) => 100) {
+    
+  }
+});
+
+/* Função pra Saber se o Scroll foi para Cima ou para Baixo */
+$(window)
